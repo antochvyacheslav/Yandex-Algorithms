3a59cb3742557ec0b6cc9cb13aa72039bf14f04d
Vyacheslav Antoch <antochvyacheslav@gmail.com>
2024-03-31 15:53:24 +0300

    added sprint1final

---
+++ sprint1final/A.cpp
@@ -0,0 +1,38 @@
+﻿//https://contest.yandex.ru/contest/22450/run-report/103388405/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector <int> GetNearestZero(const vector <int>& numberList) {
+	vector <int> result(numberList.size(), 0);
+	int j = 0;
+	for (int i = 0; i < numberList.size(); ++i, ++j) {
+		if (!numberList[i])
+			j = 0;
+		result[i] = j;
+	}
+	for (int i = numberList.size() - j; i >= 0; --i, ++j) {
+		if (!numberList[i])
+			j = 0;
+		if (j < result[i] || (i == result[i] && numberList.front()))
+			result[i] = j;
+	}
+	return result;
+}
+
+void print(const vector <int>& result) {
+	for (int x : result)
+		cout << x << " ";
+	cout << endl;
+}
+
+int main() {
+	int n;
+	cin >> n;
+	vector <int> numberList(n);
+	for (int i = 0; i < n; ++i)
+		cin >> numberList[i];
+	print(GetNearestZero(numberList));
+}
---
+++ sprint1final/B.cpp
@@ -0,0 +1,36 @@
+﻿//https://contest.yandex.ru/contest/22450/run-report/103986690/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const unsigned char MatrixSize = 4;
+const unsigned char MaximumNumber = 9;
+
+int GetNumberOfPoints(int k, const vector <string>& matrix) {
+	int result = 0;
+	vector <unsigned char> numCount(MaximumNumber, 0);
+	unsigned char asciZero = '0';
+	for (int i = 0; i < MatrixSize; ++i) {
+		for (int j = 0; j < MatrixSize; ++j) {
+			if ('.' != matrix[i][j])
+				++numCount[matrix[i][j] - asciZero - 1];
+		}
+	}
+	for (const auto& count : numCount) {
+		if (count && count <= k * 2)
+			++result;
+	}
+	return result;
+}
+
+int main() {
+	int k;
+	cin >> k;
+	vector <string> matrix(MatrixSize, "");
+	for (int i = 0; i < MatrixSize; ++i)
+		cin >> matrix[i];
+	cout << GetNumberOfPoints(k, matrix) << endl;
+}
