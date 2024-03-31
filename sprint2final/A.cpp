aff2580b64fc8a4e366fb3109dccd9235656906f
Vyacheslav Antoch <antochvyacheslav@gmail.com>
2024-03-31 15:53:41 +0300

    added sprint2final

---
+++ sprint2final/A.cpp
@@ -0,0 +1,126 @@
+﻿//https://contest.yandex.ru/contest/22781/run-report/104342770/
+
+/*
+За основу взят пример со стеком из 9 урока
+https://github.com/Yandex-Practicum/algorithms-theory-examples/blob/master/sprint2/By%20languages/C%2B%2B/lesson9code1.cpp
+
+-- ПРИНЦИП РАБОТЫ --
+head указывает на свободное место для добавления в начало дека;
+tail указывает на свободное место для добавления в конец дека;
+также как и вектор идёт от по возрастанию от 0 до max, так и head в деке идёт раньше tail, т.е. слева направо;
+при создании дека head показывает на последний индекс, а tail на начало;
+дек - deque — double ended queue - очередь с двумя концами - интерфейс, который позволяет и добавлять, и извлекать элементы с обоих концов;
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+добавление в дек стоит O(1), потому что добавление в вектор стоит O(1);
+извлечение из дека тоже стоит O(1);
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+дек, содержащий n элементов, занимает O(n) памяти
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <optional>
+
+using namespace std;
+
+class MyDeque {
+    vector<int> m_deque;
+    int m_max;
+    int m_head;
+    int m_tail;
+    int m_size;
+
+public:
+    MyDeque(int n) {
+        m_deque.resize(n);
+        m_max = n;
+        m_head = n - 1;
+        m_tail = 0;
+        m_size = 0;
+    }
+
+    bool isEmpty() const {
+        return m_size == 0;
+    }
+
+    bool isFull() const {
+        return m_size == m_max;
+    }
+
+    bool push_back(int value) {
+        if (isFull())
+            return false;
+        m_deque[m_tail] = value;
+        m_tail = (m_tail + 1) % m_max;
+        m_size += 1;
+        return true;
+    }
+
+    bool push_front(int value) {
+        if (isFull())
+            return false;
+        m_deque[m_head] = value;
+        m_head = (m_max + m_head - 1) % m_max;
+        m_size += 1;
+        return true;
+    }
+
+    optional<int> pop_back() {
+        if (isEmpty())
+            return nullopt;
+        m_tail = (m_max + m_tail - 1) % m_max;
+        int value = m_deque[m_tail];
+        m_deque[m_tail] = 0;
+        m_size -= 1;
+        return value;
+    }
+
+    optional<int> pop_front() {
+        if (isEmpty())
+            return nullopt;
+        m_head = (m_head + 1) % m_max;
+        int value = m_deque[m_head];
+        m_deque[m_head] = 0;
+        m_size -= 1;
+        return value;
+    }
+};
+
+
+int main() {
+    int n = 0;
+    int m = 0;
+    cin >> n;
+    cin >> m;
+    MyDeque deque(m);
+    vector <string> result;
+    for (int i = 0; i < n; ++i) {
+        string command;
+        cin >> command;
+        if (command == "push_back") {
+            int value;
+            cin >> value;
+            if (!deque.push_back(value))
+                result.push_back("error");
+        }
+        else if (command == "push_front") {
+            int value;
+            cin >> value;
+            if (!deque.push_front(value))
+                result.push_back("error");
+        }
+        else if (command == "pop_back") {
+            const auto pop = deque.pop_back();
+            result.push_back(pop ? to_string(pop.value()) : "error");
+        }
+        else if (command == "pop_front") {
+            const auto pop = deque.pop_front();
+            result.push_back(pop ? to_string(pop.value()) : "error");
+        }
+    }
+    for (const auto& s : result)
+        cout << s << endl;
+}
---
+++ sprint2final/B.cpp
@@ -0,0 +1,87 @@
+﻿//https://contest.yandex.ru/contest/22781/run-report/104351751/
+
+/*
+на этот сайт наткнулся, когда искал, как убрать кучу ифов и сделать мапу функций)
+https://www.geeksforgeeks.org/cpp-map-of-functions/
+эти сайты использовал, чтобы красиво реализовать математическое целочисленное деление, чтобы округление было вниз
+https://ilyachalov.livejournal.com/343124.html
+https://microkontroller.ru/programmirovanie-mikrokontrollerov-avr/pryamoy-obratnyiy-dopolnitelnyiy-kod-dvoichnogo-chisla/
+https://stackoverflow.com/questions/39304681/division-with-negative-dividend-but-rounded-towards-negative-infinity
+
+-- ПРИНЦИП РАБОТЫ --
+1. Обработка входного символа:
+- Если на вход подан операнд, он помещается на вершину стека.
+- Если на вход подан знак операции, то эта операция выполняется над требуемым количеством значений, взятых из стека в порядке добавления. Результат выполненной операции помещается на вершину стека.
+2. Если входной набор символов обработан не полностью, перейти к шагу 1.
+3. После полной обработки входного набора символов результат вычисления выражения находится в вершине стека. Если в стеке осталось несколько чисел, то надо вывести только верхний элемент.
+
+Когда а отрицательное, то для "математического целочисленного деления", которое называется "евклидово деление" в коде используется хитрый приём ~(~a / b),
+~a - интвентирование всех бит, отрицательные числа в памяти представлены в дополнительном коде, это инвертированный код плюс 1, т.е. если было число -15, то станет 14, если было 15, станет -16
+
+например, -15 / 5 для Евклидова деления должно быть -3
+a=-15, b=5
+~a=14
+~a/b=2
+~(~a/b)=-3
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+перебор строки - O(n)
+добавление в стек - O(1)
+извлечение из стека - O(1)
+арифметическая операция - O(1)
+
+итоговая временная сложность будет линейно зависеть от размера входной строки, поэтому O(n)
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+статическая память: 
+для хранения мапы из 4 элементов это O(1)
+память на стеке:
+строка на n символов O(n)
+строковый поток для обработки строки O(n)
+стек на m элементов O(m)
+
+итоговая пространственная сложность будет линейно зависеть от размера входной строки, поэтому O(n)
+*/
+
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+using Operation = int(*)(int, int);
+
+static const map<string, Operation> Operations {
+    { "+", [](int a, int b) { return a + b; } },
+    { "-", [](int a, int b) { return a - b; } },
+    { "/", [](int a, int b) { return a >= 0 ? a / b : ~(~a / b); } },
+    { "*", [](int a, int b) { return a * b; } }
+};
+
+int calculate(const string& str) {
+    stack<int> numbers;
+    stringstream ss(str);
+    string word;
+    while (ss >> word) {
+        if (const auto& operation = Operations.find(word); 
+            Operations.end() != operation) {
+            int num2 = numbers.top();
+            numbers.pop();
+            int num1 = numbers.top();
+            numbers.pop();
+            numbers.push(operation->second(num1, num2));
+        }
+        else {
+            numbers.push(stoi(word));
+        }
+    }
+    return numbers.top();
+}
+
+int main() {
+    string input;
+    getline(cin, input);
+    cout << calculate(input) << endl;
+}
